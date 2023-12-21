73a358cf3dc8bcf801f36f146d8af437f1ada238
ugurgull <ugurgul93@hotmail.com>
2023-12-21 13:34:07 +1100

    Recursive ile desene gore metot olusturma

---
+++ Recursive_desenegoremetot/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void dongu(int n){
+        System.out.println(n + " ");
+        if (n <= 0){
+            return;
+        }
+        dongu(n-5);
+        System.out.println(n + " ");
+    }
+
+
+    public static void main(String[] args) {
+        //scanner sinfimizi tanimlayalim
+        Scanner input =new Scanner(System.in);
+
+        //kullanicidan deger alalim
+        System.out.print("N sayisini giriniz : ");
+        int n = input.nextInt();
+        dongu(n);
+
+
+    }
+}
